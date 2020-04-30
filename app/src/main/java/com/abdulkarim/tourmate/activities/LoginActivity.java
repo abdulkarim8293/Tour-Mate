1b7727538a3d5aa7fe2cf55cde4c805d94d957f3
unknown <abdulkarim8293@gmail.com>
2020-04-30 23:38:41 +0600

    add login activity

--- app/src/main/java/com/abdulkarim/tourmate/activities/LoginActivity.java
+++ app/src/main/java/com/abdulkarim/tourmate/activities/LoginActivity.java
@@ -1,16 +1,20 @@
 package com.abdulkarim.tourmate.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.abdulkarim.tourmate.R;
+import com.abdulkarim.tourmate.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
     }
 }
