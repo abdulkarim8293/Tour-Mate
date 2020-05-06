29dfcf9050c275b4f1d935477d1eb97a257c44ab
unknown <abdulkarim8293@gmail.com>
2020-05-06 23:27:02 +0600

    add bottom sheet

--- app/src/main/java/com/abdulkarim/tourmate/bottom_sheet/SelectImageBottomSheet.java
+++ app/src/main/java/com/abdulkarim/tourmate/bottom_sheet/SelectImageBottomSheet.java
@@ -16,5 +16,49 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class SelectImageBottomSheet extends BottomSheetDialogFragment {
 
+    private SelectionListener mListener;
+    private LinearLayout camera,gallery;
 
+    public SelectImageBottomSheet(){
+
+    }
+
+    @SuppressLint("ValidFragment")
+    public SelectImageBottomSheet(SelectionListener mListener) {
+        this.mListener = mListener;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.bottom_sheet_select_image,container,false);
+        camera = view.findViewById(R.id.cameraImageLayoutID);
+        gallery = view.findViewById(R.id.galleryImageLayoutID);
+        onClick();
+
+        return view;
+    }
+
+    private void onClick() {
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onCameraButtonClicked();
+
+            }
+        });
+
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mListener.onGalleryButtonClicked();
+            }
+        });
+    }
+    public interface SelectionListener {
+        void onCameraButtonClicked();
+        void onGalleryButtonClicked();
+    }
 }
