7941c82fc38d1efe9f3683cc7194bd404e834298
unknown <abdulkarim8293@gmail.com>
2020-05-14 23:58:33 +0600

    update group fragment code

--- app/src/main/java/com/abdulkarim/tourmate/activities/AddNewMemberActivity.java
+++ app/src/main/java/com/abdulkarim/tourmate/activities/AddNewMemberActivity.java
@@ -1,16 +1,29 @@
 package com.abdulkarim.tourmate.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.abdulkarim.tourmate.R;
+import com.abdulkarim.tourmate.databinding.ActivityAddNewMemberBinding;
 
 public class AddNewMemberActivity extends AppCompatActivity {
+
+    private ActivityAddNewMemberBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_new_member);
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_add_new_member);
+
+        binding.backBtnIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
